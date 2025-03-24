5337d7343c01cbca376c656503a118fb272f4ea4
unknown <iman.almasbekuulu.kk@gmail.com>
2025-03-24 16:55:38 +0600

    awfsd

--- src/app/(site)/culture/games/page.tsx
+++ src/app/(site)/culture/games/page.tsx
@@ -1,5 +1,8 @@
 "use client";
-import GamesPage from "@/appPages/site/components/pages/CultureSections/Games/GamesPage";
+
+import GamesPage from "@/appPages/site/components/pages/CultureSections/games/GamesPage";
+
+// import GamesPage from "@/appPages/site/components/pages/CultureSections/Games/GamesPage";
 
 const page = () => <GamesPage />;
 
