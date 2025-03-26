275817b322feea3f525e254669ebb7c40418a59e
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2025-03-26 20:19:58 +0600

    Update

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -1,35 +1,25 @@
 "use client";
 import LoadScriptPrviders from "@/providers/LoadScriptPrviders";
-// import { NotificationProvider } from "@/providers/NotificationProvider";
-// import ProtectProvider from "@/providers/ProtectProvider";
+
 import ReduxProvider from "@/providers/ReduxProvider";
 import { RegionProvider } from "@/providers/RegionProvider";
 import { SessionProvider } from "@/providers/SessionProvider";
-// import { Session } from "next-auth";
 import React, { FC, ReactNode } from "react";
-// import { BrowserRouter } from "react-router-dom";
 
 interface RootLayoutClientProps {
   children: ReactNode;
-  // session: Session | null;
 }
 const RootLayoutClient: FC<RootLayoutClientProps> = ({ children }) => {
   return (
     <>
-      {/* <BrowserRouter> */}
 
       <ReduxProvider>
         <SessionProvider>
-          {/* <NotificationProvider> */}
-            {/* <ProtectProvider> */}
             <LoadScriptPrviders>
               <RegionProvider>{children}</RegionProvider>
             </LoadScriptPrviders>
-            {/* </ProtectProvider> */}
-          {/* </NotificationProvider> */}
         </SessionProvider>
       </ReduxProvider>
-      {/* </BrowserRouter> */}
     </>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,14 +3,12 @@ import { api } from "./api";
 import translateSlice from "./translate/translateSlice";
 import { weatherApi } from "./weatherApi";
 import { directionsApi } from "./googleMapsApi";
-// import { notificationsApi } from "./api/notifications";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     [weatherApi.reducerPath]: weatherApi.reducer,
     [directionsApi.reducerPath]: directionsApi.reducer,
-    // [notificationsApi.reducerPath]: notificationsApi.reducer, // Добавляем редюсер
     translate: translateSlice,
   },
   middleware: (getDefaultMiddleware) =>
@@ -18,7 +16,6 @@ export const store = configureStore({
       .concat(api.middleware)
       .concat(weatherApi.middleware)
       .concat(directionsApi.middleware)
-      // .concat(notificationsApi.middleware), // Добавляем middleware
 });
 
 export type RootState = ReturnType<typeof store.getState>;
